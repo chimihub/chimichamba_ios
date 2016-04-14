338a9d9b00387d48f2ec75d6c8d472c67fa9dd3a
vauxhall <zero.griffin@gmail.com>
2016-04-14 11:22:08 -0500

    title

--- chimichamba/sourceteq/source/v/login/vlogin.m
+++ chimichamba/sourceteq/source/v/login/vlogin.m
@@ -18,13 +18,21 @@
     [title setTextAlignment:NSTextAlignmentCenter];
     [title setText:NSLocalizedString(@"login_title", nil)];
     
+    UIView *titleborder = [[UIView alloc] init];
+    [titleborder setUserInteractionEnabled:NO];
+    [titleborder setClipsToBounds:YES];
+    [titleborder setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [titleborder setBackgroundColor:[UIColor colorWithWhite:0 alpha:0.3]];
+    
+    [self addSubview:titleborder];
     [self addSubview:title];
     
-    NSDictionary *views = @{@"title":title};
+    NSDictionary *views = @{@"title":title, @"titleborder":titleborder};
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[title]-0-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-80-[title(30)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-40-[titleborder]-40-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-70-[title]-10-[titleborder(1)]" options:0 metrics:metrics views:views]];
     
     return self;
 }
