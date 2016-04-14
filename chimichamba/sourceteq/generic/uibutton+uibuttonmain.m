1c4b579f28f9cd8760735d44daa3171edac42fd1
vauxhall <zero.griffin@gmail.com>
2016-04-14 11:52:59 -0500

    buttons

--- chimichamba/sourceteq/generic/uibutton+uibuttonmain.m
+++ chimichamba/sourceteq/generic/uibutton+uibuttonmain.m
@@ -1,15 +1,39 @@
 #import "uibutton+uibuttonmain.h"
+#import "uicolor+uicolormain.h"
+
+static CGFloat const radius = 6;
 
 @implementation UIButton (uibuttonmain)
 
 +(instancetype)main:(NSString*)title
 {
+    UIButton *button = [UIButton button:title];
+    [button setBackgroundColor:[UIColor main]];
     
+    return button;
 }
 
 +(instancetype)second:(NSString*)title
 {
+    UIButton *button = [UIButton button:title];
+    [button setBackgroundColor:[UIColor second]];
     
+    return button;
+}
+
+#pragma mark private
+
++(instancetype)button:(NSString*)title
+{
+    UIButton *button = [[UIButton alloc] init];
+    [button setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [button setClipsToBounds:YES];
+    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor colorWithWhite:1 alpha:0.2] forState:UIControlStateHighlighted];
+    [button setTitle:title forState:UIControlStateNormal];
+    [button.layer setCornerRadius:radius];
+    
+    return button;
 }
 
 @end
