668c88500c5c3acc38972b58f33340a2c547acbb
vauxhall <zero.griffin@gmail.com>
2016-04-14 14:56:15 -0500

    profile

--- chimichamba/sourceteq/source/v/profile/vprofile.m
+++ chimichamba/sourceteq/source/v/profile/vprofile.m
@@ -15,6 +15,16 @@
 {
     self = [super init:controller];
     
+    vprofilebar *bar = [[vprofilebar alloc] init:controller];
+    
+    [self addSubview:bar];
+    
+    NSDictionary *views = @{@"bar":bar};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[bar]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[bar]" options:0 metrics:metrics views:views]];
+    
     return self;
 }
 
