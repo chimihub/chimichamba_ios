7d59bede4c3500dcad7a752dd8d6199554d012c6
vauxhall <zero.griffin@gmail.com>
2016-04-14 17:28:12 -0500

    picker controller

--- chimichamba/sourceteq/source/c/profile/cprofile.m
+++ chimichamba/sourceteq/source/c/profile/cprofile.m
@@ -33,6 +33,22 @@
     return NO;
 }
 
+#pragma mark functionality
+
+-(void)presentpicker:(UIImagePickerController*)picker
+{
+    if(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+    {
+        UIPopoverController *popover = [[UIPopoverController alloc] initWithContentViewController:picker];
+        self.popover = popover;
+        [popover presentPopoverFromRect:CGRectMake(self.view.picture.bounds.size.width, self.view.bounds.size.height / 2, 1, 1) inView:self.view.picture permittedArrowDirections:UIPopoverArrowDirectionLeft animated:YES];
+    }
+    else
+    {
+        [self presentViewController:picker animated:YES completion:nil];
+    }
+}
+
 #pragma mark public
 
 -(void)back
@@ -42,12 +58,20 @@
 
 -(void)picturecamera
 {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
     
+    [self presentpicker:picker];
 }
 
 -(void)picturelibrary
 {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
     
+    [self presentpicker:picker];
 }
 
 #pragma mark -
