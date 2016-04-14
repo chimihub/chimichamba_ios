cb09c5ff69b3e82610ccc1506223c053dfba6342
vauxhall <zero.griffin@gmail.com>
2016-04-14 17:34:44 -0500

    view profile name

--- chimichamba/sourceteq/source/v/profile/vprofile.h
+++ chimichamba/sourceteq/source/v/profile/vprofile.h
@@ -1,10 +1,12 @@
 #import "vchimi.h"
 #import "vprofilepicture.h"
+#import "vprofilename.h"
 
 @interface vprofile:vchimi
 
 -(void)updatepicture:(UIImage*)image;
 
 @property(weak, nonatomic)vprofilepicture *picture;
+@property(weak, nonatomic)vprofilename *name;
 
 @end
---
+++ chimichamba/sourceteq/source/v/profile/vprofilename.m
@@ -0,0 +1,13 @@
+//
+//  vprofilename.m
+//  chimichamba
+//
+//  Created by zero on 4/14/16.
+//  Copyright © 2016 chimi. All rights reserved.
+//
+
+#import "vprofilename.h"
+
+@implementation vprofilename
+
+@end
