9136d0d0f845994caf16c27721cbb4f3bc02523a
vauxhall <zero.griffin@gmail.com>
2016-04-14 17:17:37 -0500

    actions alert

--- chimichamba/sourceteq/source/v/profile/vprofilealert.m
+++ chimichamba/sourceteq/source/v/profile/vprofilealert.m
@@ -14,19 +14,27 @@
     
     vblur *blur = [vblur light:NO];
     
+    UIButton *buttoncamera = [UIButton main:NSLocalizedString(@"profile_alert_button_camera", nil)];
+    [buttoncamera addTarget:self action:@selector(actioncamera:) forControlEvents:UIControlEventTouchUpInside];
+    
+    UIButton *buttonlibrary = [UIButton main:NSLocalizedString(@"profile_alert_button_library", nil)];
+    [buttonlibrary addTarget:self action:@selector(actionlibrary:) forControlEvents:UIControlEventTouchUpInside];
+    
     UIButton *buttoncancel = [UIButton main:NSLocalizedString(@"profile_alert_button_cancel", nil)];
     [buttoncancel addTarget:self action:@selector(actioncancel:) forControlEvents:UIControlEventTouchUpInside];
     
     [self addSubview:blur];
     [self addSubview:buttoncancel];
     
-    NSDictionary *views = @{@"blur":blur, @"buttoncancel":buttoncancel};
+    NSDictionary *views = @{@"blur":blur, @"buttoncancel":buttoncancel, @"buttoncamera":buttoncamera, @"buttonlibrary":buttonlibrary};
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[blur]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[blur]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-40-[buttoncamera]-40-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-40-[buttonlibrary]-40-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-40-[buttoncancel]-40-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-150-[buttoncancel]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-150-[buttoncamera]-20-[buttonlibrary]-20-[buttoncancel]" options:0 metrics:metrics views:views]];
     
     return self;
 }
@@ -36,6 +44,16 @@
 -(void)actioncancel:(UIButton*)button
 {
     [self removeFromSuperview];
+}
+
+-(void)actioncamera:(UIButton*)button
+{
+    [self.controller picturecamera];
+}
+
+-(void)actionlibrary:(UIButton*)button
+{
+    [self.controller picturelibrary];
 }
 
 @end
