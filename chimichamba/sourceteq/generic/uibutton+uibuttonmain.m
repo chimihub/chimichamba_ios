4e3d2b82c410a5dfc547b67831096b8ecadb4798
vauxhall <zero.griffin@gmail.com>
2016-04-14 11:38:59 -0500

    button category

---
+++ chimichamba/sourceteq/generic/uibutton+uibuttonmain.m
@@ -0,0 +1,13 @@
+//
+//  uibutton+uibuttonmain.m
+//  chimichamba
+//
+//  Created by zero on 4/14/16.
+//  Copyright © 2016 chimi. All rights reserved.
+//
+
+#import "uibutton+uibuttonmain.h"
+
+@implementation uibutton_uibuttonmain
+
+@end
