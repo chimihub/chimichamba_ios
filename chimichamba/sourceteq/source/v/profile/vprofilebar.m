12278e6b82089d0c5cf1c1ebd4336d31e88eb372
vauxhall <zero.griffin@gmail.com>
2016-04-14 15:21:35 -0500

    profile

--- chimichamba/sourceteq/source/v/profile/vprofilebar.m
+++ chimichamba/sourceteq/source/v/profile/vprofilebar.m
@@ -12,6 +12,35 @@
     [self setBackgroundColor:[UIColor main]];
     self.controller = controller;
     
+    UIButton *buttonback = [[UIButton alloc] init];
+    [buttonback setClipsToBounds:YES];
+    [buttonback setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [buttonback setImage:[[UIImage imageNamed:@"generic_back"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] forState:UIControlStateNormal];
+    [buttonback setImage:[[UIImage imageNamed:@"generic_back"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate] forState:UIControlStateHighlighted];
+    [buttonback.imageView setTintColor:[UIColor colorWithWhite:1 alpha:0.2]];
+    [buttonback.imageView setContentMode:UIViewContentModeScaleAspectFit];
+    [buttonback addTarget:self action:@selector(actionback:) forControlEvents:UIControlEventTouchUpInside];
+    
+    UILabel *label = [[UILabel alloc] init];
+    [label setBackgroundColor:[UIColor clearColor]];
+    [label setUserInteractionEnabled:NO];
+    [label setFont:[UIFont systemFontOfSize:16]];
+    [label setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [label setTextAlignment:NSTextAlignmentCenter];
+    [label setTextColor:[UIColor whiteColor]];
+    [label setText:NSLocalizedString(@"profile_title", nil)];
+    
+    [self addSubview:label];
+    [self addSubview:buttonback];
+    
+    NSDictionary *views = @{@"buttonback":buttonback, @"label":label};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[buttonback(60)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[label]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[buttonback(55)]-(-5)-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[label(45)]-0-|" options:0 metrics:metrics views:views]];
+    
     return self;
 }
 
@@ -26,7 +55,7 @@
 
 -(void)actionback:(UIButton*)button
 {
-    
+    [self.controller back];
 }
 
 @end
