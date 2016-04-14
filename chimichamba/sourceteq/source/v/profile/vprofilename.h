1aa0f4f2887a5cb9c87b81a3234d1f0e28cdfd45
vauxhall <zero.griffin@gmail.com>
2016-04-14 17:43:43 -0500

    text field

--- chimichamba/sourceteq/source/v/profile/vprofilename.h
+++ chimichamba/sourceteq/source/v/profile/vprofilename.h
@@ -1,13 +1,7 @@
-//
-//  vprofilename.h
-//  chimichamba
-//
-//  Created by zero on 4/14/16.
-//  Copyright © 2016 chimi. All rights reserved.
-//
+#import <UIKit/UIKit.h>
 
-#import <Foundation/Foundation.h>
+@interface vprofilename:UIView<UITextFieldDelegate>
 
-@interface vprofilename : NSObject
+@property(weak, nonatomic)UITextField *field;
 
 @end
--- chimichamba/sourceteq/source/v/profile/vprofilename.m
+++ chimichamba/sourceteq/source/v/profile/vprofilename.m
@@ -1,13 +1,50 @@
-//
-//  vprofilename.m
-//  chimichamba
-//
-//  Created by zero on 4/14/16.
-//  Copyright © 2016 chimi. All rights reserved.
-//
-
 #import "vprofilename.h"
 
 @implementation vprofilename
+
+-(instancetype)init
+{
+    self = [super init];
+    [self setClipsToBounds:YES];
+    [self setBackgroundColor:[UIColor clearColor]];
+    [self setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    UITextField *field = [[UITextField alloc] init];
+    [field setAutocapitalizationType:UITextAutocapitalizationTypeWords];
+    [field setAutocorrectionType:UITextAutocorrectionTypeNo];
+    [field setBackgroundColor:[UIColor clearColor]];
+    [field setBorderStyle:UITextBorderStyleNone];
+    [field setClearButtonMode:UITextFieldViewModeNever];
+    [field setDelegate:self];
+    [field setFont:[UIFont systemFontOfSize:22]];
+    [field setKeyboardType:UIKeyboardTypeAlphabet];
+    [field setKeyboardAppearance:UIKeyboardAppearanceLight];
+    [field setPlaceholder:NSLocalizedString(@"profile_name_placeholder", nil)];
+    [field setReturnKeyType:UIReturnKeyDone];
+    [field setSpellCheckingType:UITextSpellCheckingTypeNo];
+    [field setTextColor:[UIColor blackColor]];
+    [field setTintColor:[UIColor blackColor]];
+    [field setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    [self addSubview:field];
+    
+    NSDictionary *views = @{@"field":field};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[field]-10-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[field]-0-|" options:0 metrics:metrics views:views]];
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark field del
+
+-(BOOL)textFieldShouldReturn:(UITextField*)field
+{
+    [field resignFirstResponder];
+    
+    return YES;
+}
 
 @end
