4b3fa1954e1a8a6e62e3c3ac9ab164ec85d6f9ce
vauxhall <zero.griffin@gmail.com>
2016-04-14 12:05:43 -0500

    view profile

---
+++ chimichamba/sourceteq/source/v/profile/vprofile.h
@@ -0,0 +1,13 @@
+//
+//  vprofile.h
+//  chimichamba
+//
+//  Created by zero on 4/14/16.
+//  Copyright © 2016 chimi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface vprofile : NSObject
+
+@end
---
+++ chimichamba/sourceteq/source/v/profile/vprofile.m
@@ -0,0 +1,13 @@
+//
+//  vprofile.m
+//  chimichamba
+//
+//  Created by zero on 4/14/16.
+//  Copyright © 2016 chimi. All rights reserved.
+//
+
+#import "vprofile.h"
+
+@implementation vprofile
+
+@end
