514b1258cfa25a26e8fd335f9c86fb246c5e6b02
vauxhall <zero.griffin@gmail.com>
2016-04-14 12:35:49 -0500

    motto

--- chimichamba/sourceteq/source/v/login/vlogin.m
+++ chimichamba/sourceteq/source/v/login/vlogin.m
@@ -1,5 +1,6 @@
 #import "vlogin.h"
 #import "uibutton+uibuttonmain.h"
+#import "uicolor+uicolormain.h"
 
 @implementation vlogin
 
@@ -16,11 +17,27 @@
     [title setTextAlignment:NSTextAlignmentCenter];
     [title setText:NSLocalizedString(@"login_title", nil)];
     
+    UILabel *motto = [[UILabel alloc] init];
+    [motto setBackgroundColor:[UIColor clearColor]];
+    [motto setUserInteractionEnabled:NO];
+    [motto setFont:[UIFont systemFontOfSize:16]];
+    [motto setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [motto setTextColor:[UIColor second]];
+    [motto setTextAlignment:NSTextAlignmentCenter];
+    [motto setText:NSLocalizedString(@"app_motto", nil)];
+    
     UIView *titleborder = [[UIView alloc] init];
     [titleborder setUserInteractionEnabled:NO];
     [titleborder setClipsToBounds:YES];
     [titleborder setTranslatesAutoresizingMaskIntoConstraints:NO];
     [titleborder setBackgroundColor:[UIColor colorWithWhite:0 alpha:0.3]];
+    
+    UIImageView *logo = [[UIImageView alloc] init];
+    [logo setUserInteractionEnabled:NO];
+    [logo setClipsToBounds:YES];
+    [logo setContentMode:UIViewContentModeScaleAspectFit];
+    [logo setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [logo setImage:[UIImage imageNamed:@"generic_logo"]];
     
     UIButton *buttonfacebook = [UIButton main:NSLocalizedString(@"login_btn_facebook", nil)];
     self.buttonfacebook = buttonfacebook;
@@ -29,18 +46,22 @@
     self.buttonemail = buttonemail;
     
     [self addSubview:titleborder];
+    [self addSubview:motto];
     [self addSubview:title];
     [self addSubview:buttonfacebook];
     [self addSubview:buttonemail];
+    [self addSubview:logo];
     
-    NSDictionary *views = @{@"title":title, @"titleborder":titleborder, @"btnface":buttonfacebook, @"btnemail":buttonemail};
+    NSDictionary *views = @{@"title":title, @"titleborder":titleborder, @"btnface":buttonfacebook, @"btnemail":buttonemail, @"logo":logo, @"motto":motto};
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[title]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[motto]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-50-[titleborder]-50-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[btnface]-20-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[btnemail]-20-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-70-[title]-10-[titleborder(1)]-150-[btnface(50)]-20-[btnemail(50)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[logo]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-70-[title(30)]-10-[titleborder(1)]-40-[logo(85)]-10-[motto(20)]-80-[btnface(50)]-20-[btnemail(50)]" options:0 metrics:metrics views:views]];
     
     return self;
 }
