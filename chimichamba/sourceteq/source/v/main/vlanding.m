7a82b7de84bf250bed3334f2df6d5d2665ad50bd
vauxhall <zero.griffin@gmail.com>
2016-04-14 10:48:31 -0500

    view landing

---
+++ chimichamba/sourceteq/source/v/main/vlanding.m
@@ -0,0 +1,14 @@
+#import "vlanding.h"
+
+@implementation vlanding
+
+-(instancetype)init
+{
+    self = [super init];
+    [self setClipsToBounds:YES];
+    [self setBackgroundColor:[UIColor whiteColor]];
+    
+    return self;
+}
+
+@end
