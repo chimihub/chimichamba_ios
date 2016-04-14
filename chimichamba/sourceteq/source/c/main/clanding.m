6c11f9217ca53692c29f7b7ffcacfbdadc813f4c
vauxhall <zero.griffin@gmail.com>
2016-04-14 10:56:44 -0500

    controller landing

--- chimichamba/sourceteq/source/c/main/clanding.m
+++ chimichamba/sourceteq/source/c/main/clanding.m
@@ -1,5 +1,19 @@
 #import "clanding.h"
+#import "vlanding.h"
+
+@interface vlanding ()
+
+@property(strong, nonatomic)vlanding *view;
+
+@end
 
 @implementation clanding
+
+@dynamic view;
+
+-(void)loadView
+{
+    self.view = [[vlanding alloc] init];
+}
 
 @end
