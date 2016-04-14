b157a10f2ee44a338f5f24240fe23e1199a07db7
vauxhall <zero.griffin@gmail.com>
2016-04-14 18:03:42 -0500

    view employee

---
+++ chimichamba/sourceteq/source/c/employee/cemployee.m
@@ -0,0 +1,13 @@
+//
+//  cemployee.m
+//  chimichamba
+//
+//  Created by zero on 4/14/16.
+//  Copyright © 2016 chimi. All rights reserved.
+//
+
+#import "cemployee.h"
+
+@implementation cemployee
+
+@end
---
+++ chimichamba/sourceteq/source/v/employee/vemployee.m
@@ -0,0 +1,13 @@
+//
+//  vemployee.m
+//  chimichamba
+//
+//  Created by zero on 4/14/16.
+//  Copyright © 2016 chimi. All rights reserved.
+//
+
+#import "vemployee.h"
+
+@implementation vemployee
+
+@end
