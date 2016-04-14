e474415f542d7756c05a2ce5262dfcbf6f615dd4
vauxhall <zero.griffin@gmail.com>
2016-04-14 12:05:27 -0500

    controller profile

---
+++ chimichamba/sourceteq/source/c/profile/cprofile.m
@@ -0,0 +1,13 @@
+//
+//  cprofile.m
+//  chimichamba
+//
+//  Created by zero on 4/14/16.
+//  Copyright © 2016 chimi. All rights reserved.
+//
+
+#import "cprofile.h"
+
+@implementation cprofile
+
+@end
