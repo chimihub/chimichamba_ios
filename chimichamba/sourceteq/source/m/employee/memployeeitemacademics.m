b3020bf5a09531016574e687cd4f9a59530f7604
vauxhall <zero.griffin@gmail.com>
2016-04-18 09:19:53 -0500

    model academics

--- chimichamba/sourceteq/source/m/employee/memployeeitemacademics.m
+++ chimichamba/sourceteq/source/m/employee/memployeeitemacademics.m
@@ -1,5 +1,17 @@
 #import "memployeeitemacademics.h"
+#import "vemployeecellacademics.h"
+
+static NSUInteger const cellheight = 100;
 
 @implementation memployeeitemacademics
+
+-(instancetype)init
+{
+    self = [super init];
+    self.cellclass = [vemployeecellacademics class];
+    self.height = cellheight;
+    
+    return self;
+}
 
 @end
