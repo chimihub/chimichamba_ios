97131a19c5dca3c31a805b68af46b49e6de89389
vauxhall <zero.griffin@gmail.com>
2016-04-18 17:29:39 -0500

    collectiion cell

--- chimichamba/sourceteq/source/v/employeeskill/vemployeeskillcell.h
+++ chimichamba/sourceteq/source/v/employeeskill/vemployeeskillcell.h
@@ -1,5 +1,10 @@
 #import <UIKit/UIKit.h>
+#import "mskillitem.h"
 
 @interface vemployeeskillcell:UICollectionViewCell
+
+-(void)config:(mskillitem*)model;
+
+@property(weak, nonatomic)UILabel *label;
 
 @end
--- chimichamba/sourceteq/source/v/employeeskill/vemployeeskillcell.m
+++ chimichamba/sourceteq/source/v/employeeskill/vemployeeskillcell.m
@@ -1,4 +1,5 @@
 #import "vemployeeskillcell.h"
+#import "uicolor+uicolormain.h"
 
 @implementation vemployeeskillcell
 
@@ -6,9 +7,59 @@
 {
     self = [super initWithFrame:frame];
     [self setClipsToBounds:YES];
-    [self setBackgroundColor:[UIColor whiteColor]];
+
+    UILabel *label = [[UILabel alloc] init];
+    [label setBackgroundColor:[UIColor clearColor]];
+    [label setUserInteractionEnabled:NO];
+    [label setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [label setFont:[UIFont systemFontOfSize:18]];
+    self.label = label;
+    
+    [self addSubview:label];
+    
+    NSDictionary *views = @{@"label":label};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[label]-20-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[label]-0-|" options:0 metrics:metrics views:views]];
     
     return self;
+}
+
+-(void)setSelected:(BOOL)selected
+{
+    [super setSelected:selected];
+    [self hover];
+}
+
+-(void)setHighlighted:(BOOL)highlighted
+{
+    [super setHighlighted:highlighted];
+    [self hover];
+}
+
+#pragma mark functionality
+
+-(void)hover
+{
+    if(self.isSelected || self.isHighlighted)
+    {
+        [self.label setTextColor:[UIColor whiteColor]];
+        [self setBackgroundColor:[UIColor main]];
+    }
+    else
+    {
+        [self.label setTextColor:[UIColor colorWithWhite:0 alpha:0.7]];
+        [self setBackgroundColor:[UIColor whiteColor]];
+    }
+}
+
+#pragma mark public
+
+-(void)config:(mskillitem*)model
+{
+    [self.label setText:model.name];
+    [self hover];
 }
 
 @end
