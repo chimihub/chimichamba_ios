61e6d101b741fa215810e22e48c248467651c6e4
vauxhall <zero.griffin@gmail.com>
2016-04-18 16:11:08 -0500

    models

--- chimichamba/sourceteq/source/m/skill/mskillcatprofession.m
+++ chimichamba/sourceteq/source/m/skill/mskillcatprofession.m
@@ -1,11 +1,18 @@
 #import "mskillcatprofession.h"
+#import "mskillitemdesigner.h"
+#import "mskillitemdoctor.h"
+#import "mskillitemprofessor.h"
 
 @implementation mskillcatprofession
 
 -(instancetype)init
 {
     self = [super init];
-    self.items = @[];
+    self.items = @[
+                   [[mskillitemdesigner alloc] init],
+                   [[mskillitemdoctor alloc] init],
+                   [[mskillitemprofessor alloc] init]
+                   ];
     
     return self;
 }
