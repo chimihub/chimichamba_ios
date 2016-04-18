dd0972e7aa3c5c495fe7f39583de01ca306716fc
vauxhall <zero.griffin@gmail.com>
2016-04-18 17:19:39 -0500

    header

--- chimichamba/sourceteq/source/v/employeeskill/vemployeeskillheader.h
+++ chimichamba/sourceteq/source/v/employeeskill/vemployeeskillheader.h
@@ -1,5 +1,10 @@
 #import <UIKit/UIKit.h>
+#import "mskillcat.h"
 
 @interface vemployeeskillheader:UICollectionReusableView
+
+-(void)config:(mskillcat*)model;
+
+@property(weak, nonatomic)UILabel *label;
 
 @end
--- chimichamba/sourceteq/source/v/employeeskill/vemployeeskillheader.m
+++ chimichamba/sourceteq/source/v/employeeskill/vemployeeskillheader.m
@@ -2,4 +2,35 @@
 
 @implementation vemployeeskillheader
 
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    [self setClipsToBounds:YES];
+    [self setBackgroundColor:[UIColor clearColor]];
+    
+    UILabel *label = [[UILabel alloc] init];
+    [label setUserInteractionEnabled:NO];
+    [label setBackgroundColor:[UIColor clearColor]];
+    [label setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [label setFont:[UIFont boldSystemFontOfSize:16]];
+    [label setTextColor:[UIColor colorWithWhite:0 alpha:0.5]];
+    self.label = label;
+    
+    [self addSubview:label];
+    
+    NSDictionary *views = @{@"label":label};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"" options:0 metrics:metrics views:views]];
+    
+    return self;
+}
+
+#pragma mark public
+
+-(void)config:(mskillcat*)model
+{
+    [self.label setText:model.name];
+}
+
 @end
