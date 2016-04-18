448c133646d3fc3aca680eeff28122b56cbbf4e9
vauxhall <zero.griffin@gmail.com>
2016-04-18 15:58:07 -0500

    model skills

---
+++ chimichamba/sourceteq/source/m/skill/mskillcat.m
@@ -0,0 +1,13 @@
+//
+//  mskillcat.m
+//  chimichamba
+//
+//  Created by zero on 4/18/16.
+//  Copyright © 2016 chimi. All rights reserved.
+//
+
+#import "mskillcat.h"
+
+@implementation mskillcat
+
+@end
