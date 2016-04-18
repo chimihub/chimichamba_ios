6097c1abe5c3b96b7181d38a124a25b4923980ca
vauxhall <zero.griffin@gmail.com>
2016-04-18 17:38:42 -0500

    select current

--- chimichamba/sourceteq/source/v/employeeskill/vemployeeskill.m
+++ chimichamba/sourceteq/source/v/employeeskill/vemployeeskill.m
@@ -57,7 +57,62 @@ static NSUInteger const searchheight = 80;
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[bar]-0-[col]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
     
+    [self selectcurrent];
+    
     return self;
+}
+
+#pragma mark functionality
+
+-(void)selectcurrent
+{
+    mskillitem *current = self.controller.model.skill;
+    
+    if(current)
+    {
+        NSIndexPath *indexpath;
+        NSUInteger countsections = self.model.cats.count;
+        
+        for(NSUInteger i = 0; i < countsections; i++)
+        {
+            NSUInteger countitems = self.model.cats[i].items.count;
+            
+            for(NSUInteger j = 0; j < countitems; j++)
+            {
+                mskillitem *initem = self.model.cats[i].items[j];
+                
+                if(initem == current)
+                {
+                    indexpath = [NSIndexPath indexPathForItem:j inSection:i + 1];
+                    j = countitems;
+                    i = countsections;
+                }
+            }
+        }
+        
+        if(indexpath)
+        {
+            [self.collection selectItemAtIndexPath:indexpath animated:NO scrollPosition:UICollectionViewScrollPositionTop];
+        }
+    }
+}
+
+-(mskillitem*)modelforindex:(NSIndexPath*)index
+{
+    NSUInteger item = index.item;
+    NSUInteger section = index.section;
+    mskillitem *model;
+    
+    if(section)
+    {
+        model = self.model.cats[section - 1].items[item];
+    }
+    else
+    {
+        model = self.results[item];
+    }
+    
+    return model;
 }
 
 #pragma mark -
@@ -144,9 +199,19 @@ static NSUInteger const searchheight = 80;
 
 -(UICollectionViewCell*)collectionView:(UICollectionView*)col cellForItemAtIndexPath:(NSIndexPath*)index
 {
+    mskillitem *model = [self modelforindex:index];
     vemployeeskillcell *cell = [col dequeueReusableCellWithReuseIdentifier:skillcellid forIndexPath:index];
+
+    [cell config:model];
     
     return cell;
+}
+
+-(void)collectionView:(UICollectionView*)col didSelectItemAtIndexPath:(NSIndexPath*)index
+{
+    mskillitem *model = [self modelforindex:index];
+    self.controller.model.skill = model;
+    [self.controller back];
 }
 
 @end
